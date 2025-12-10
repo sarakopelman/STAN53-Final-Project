5331459405d091fdbdf8811147da27e52f95c813
Markusgerholm <markus.gerholm1@gmail.com>
2025-12-10 09:27:15 +0100

    File with data preparation

---
+++ Code/sweden_returns_washed.r
@@ -0,0 +1,31 @@
+## Loading data
+
+library(dplyr)
+
+df <- read.csv(file.path("..", "Data", "sweden_returns_merged.csv"))
+
+## Choosing time period: 2019 - 2021
+
+df_2019_2021 <- df %>%
+  mutate(X = as.Date(X)) %>%   # if this gives NA, see note below
+  filter(X >= as.Date("2019-01-01"),
+         X <= as.Date("2021-12-31"))
+
+
+## Checking for missing values
+
+na_counts <- sort(colSums(is.na(df_2019_2021)), decreasing = TRUE)
+na_counts[na_counts > 0]
+
+## Removing all assets that have number of NA > 1 for time period
+
+na_counts <- colSums(is.na(df_2019_2021))
+keep_cols <- names(na_counts)[na_counts <= 1]
+keep_cols <- union("X", setdiff(keep_cols, "X"))  # ensure X stays first/kept
+df_clean <- df_2019_2021 %>% select(all_of(keep_cols))
+
+## Interpolating/Extrapolating the two missing values for the two assets with only one value missing
+
+df_clean$INCOAX.ST[1] <- 0 # assign 0 as if stock did not move
+df_clean$RO.ST[219] <- 0 # assign 0 as if stock did not move
+df_clean <- df_clean[, setdiff(names(df_clean), "BOTX.ST")] # removing this stock since it was only zeros
