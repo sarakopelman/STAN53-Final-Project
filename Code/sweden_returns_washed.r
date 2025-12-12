2dd043ae546e08e075686a5f40353e033d0471a9
Markusgerholm <markus.gerholm1@gmail.com>
2025-12-12 13:03:56 +0100

    Forgot to remove 2021 from washed data

--- Code/sweden_returns_washed.r
+++ Code/sweden_returns_washed.r
@@ -5,25 +5,25 @@ library(dplyr)
 df <- read.csv("Data/sweden_returns_merged.csv")
 
 colnames(df)[colnames(df) == "X2CUREX.ST"] <- "2CUREX.ST"
-## Choosing time period: 2019 - 2021
+## Choosing time period: 2019 - 2022
 
 df_2019_2022 <- df %>%
-  mutate(X = as.Date(X)) %>%   # if this gives NA, see note below
+  mutate(X = as.Date(X)) %>%   # if this gives NA, see note below 
   filter(X >= as.Date("2019-01-01"),
          X <= as.Date("2022-12-31"))
 
 
 ## Checking for missing values
 
-na_counts <- sort(colSums(is.na(df_2019_2021)), decreasing = TRUE)
+na_counts <- sort(colSums(is.na(df_2019_2022)), decreasing = TRUE)
 na_counts[na_counts > 0]
 
 ## Removing all assets that have number of NA > 1 for time period
 
-na_counts <- colSums(is.na(df_2019_2021))
+na_counts <- colSums(is.na(df_2019_2022))
 keep_cols <- names(na_counts)[na_counts <= 1]
 keep_cols <- union("X", setdiff(keep_cols, "X"))  # ensure X stays first/kept
-df_clean <- df_2019_2021 %>% select(all_of(keep_cols))
+df_clean <- df_2019_2022 %>% select(all_of(keep_cols))
 
 ## Interpolating/Extrapolating the two missing values for the two assets with only one value missing
 
